9ff1c581e0aca0c5ffca1c7bc2c1d6b5cef33961
Wei Zeng <wzeng4wk@gmail.com>
2019-04-27 17:38:55 -0700

    add page

--- create_db.js
+++ create_db.js
@@ -1,3 +1,17 @@
 //create sqlite db
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database('ezhelp.db');
+
+db.serialize(() => {
+  // create a new database table:
+  db.run("CREATE TABLE request_info (name TEXT, phone TEXT, location TEXT)");
+
+  console.log('successfully created the request_info table in ezhelp.db');
+
+  // print them out to confirm their contents:
+  db.each("SELECT name, phone, location FROM request_info", (err, row) => {
+      console.log(row.name + ": " + row.phone + ' - ' + row.location);
+  });
+});
+
+db.close();
--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const db = new sqlite3.Database('ezhelp.db');
 
 app.use(express.static('static_files'));
 
+
+
 app.listen(3000, () => {
   console.log('Server started at http://localhost:3000/');
 });
