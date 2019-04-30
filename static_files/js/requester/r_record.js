c33b3e4961c51649ce7de1185d65288955d96ea2
ste <ste@MacBook-Pro-8.attlocal.net>
2019-04-29 20:31:36 -0700

    add r_record and fix r_report

--- static_files/js/requester/r_record.js
+++ static_files/js/requester/r_record.js
@@ -1,14 +1,41 @@
+
 $(document).ready(() => {
+  let all_records;
   $.ajax({
-    url: 'request_info',
+    url: '../../request_info',
     type: 'GET',
     dataType : 'json',
     success: (data) => {
       console.log('You received some data!', data);
-      $('#lqz_status').html('All users: ' + data);
-    },
-  })
-});
+      all_records = data;
+
+      const source = $("#entry-template").html();
+      const template = Handlebars.compile(source);
+      const parentDiv = $("#templatedProjects");
+
+      for (const record of all_records) {
+          const curData = record;
+          const curHtml = template(curData);
+          parentDiv.append(curHtml);
+          console.log(curData)
+        }
+      }
+    });
+
+    $('#lqz_new_request').click(() => {
+      window.location = "r_request.html";
+    });
+
+    $('#lqz_home').click(() => {
+      window.location = "../index.html";
+    });
+
+    $('#lqz_refresh').click(() => {
+      document.location.reload();
+    });
+
+  });
+
 
 $(document).ajaxError(() => {
   $('#status').html('Error: unknown ajaxError!');
