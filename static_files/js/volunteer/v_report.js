96e02c7d8285c14808e81f6ff395b552a653839a
Wei Zeng <wzeng4wk@gmail.com>
2019-04-30 15:43:19 -0700

    btn functionality in v_report -> v_record

---
+++ static_files/js/volunteer/v_report.js
@@ -0,0 +1,12 @@
+$(document).ready(() => {
+
+  $("#zw_v_report_cancel").click(() => {
+    console.log("cancel clicked!");
+    window.location = "v_record.html";
+  })
+
+  $("#zw_v_report_submit").click(() => {
+    console.log("submit clicked");
+    window.location = "v_record.html";
+  })
+});
