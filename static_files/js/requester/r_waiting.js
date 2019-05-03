3f555f1c3ff5332ca0a7830e19031717ebc64ea8
Wei Zeng <wzeng4wk@gmail.com>
2019-05-03 00:05:31 -0700

    fix link from waiting page to index, fix text alignment

--- static_files/js/requester/r_waiting.js
+++ static_files/js/requester/r_waiting.js
@@ -5,7 +5,7 @@ $(document).ready(() => {
   $('#zsy_cancel').click(() => {
     console.log("Cancel clicked");
     if(confirm("Are you sure to cancel?")) {
-      window.location = "../index.html";
+      window.location = "../../index.html";
     }
   });
 
