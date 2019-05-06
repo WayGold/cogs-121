4161c377b86591dd32227fa268f1de3d92de9b96
zsy <zsy@zsydeMacBook-Pro.local>
2019-05-06 14:14:10 -0700

    fix waiting.html cancel_button doesn't work

--- static_files/js/requester/waiting.js
+++ static_files/js/requester/waiting.js
@@ -5,3 +5,12 @@ $(document).ready(() => {
   });
 
 })
+$(document).ready(() => {
+  $('#zsy_cancel').click(() => {
+    console.log("Cancel clicked");
+    if(confirm("Are you sure to cancel?")) {
+      window.location = "../../index.html";
+    }
+  });
+
+})
