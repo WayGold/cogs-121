58adcea0eb54cdf356fb3c20be8be5665d2f8fd7
ste <ste@MacBook-Pro-10.attlocal.net>
2019-05-11 19:18:16 -0700

    Fix v_task.html

--- static_files/js/volunteer/v_task.js
+++ static_files/js/volunteer/v_task.js
@@ -1,9 +1,55 @@
+
+
 $(document).ready(() => {
-  $('.lqz_report').click(() => {
-    window.location = "v_report.html";
+  let all_records;
+  $.ajax({
+    url: '../../request_info',
+    type: 'GET',
+    dataType : 'json',
+    success: (data) => {
+      console.log('You received some data!', data);
+      all_records = data;
+
+      const source = $("#entry-template").html();
+      const template = Handlebars.compile(source);
+      const parentDiv = $("#templatedProjects");
+
+      for (const record of all_records) {
+          const curData = record;
+          const curHtml = template(curData);
+          parentDiv.append(curHtml);
+
+
+          $('.lqz_report').click(() => {
+            window.location = "v_report.html";
+          });
+
+          $('.record').click(() => {
+            window.location = "v_taskinfo.html";
+          });
+
+            $('.lqz_accept').click(() => {
+              console.log("Accepted.");
+              window.location = "v_taskinfo.html";
+            });
+
+
+          console.log(curData)
+        }
+      }
+    });
+
+    $('#lqz_find').click(() => {
+      window.location = "v_task.html";
+    });
+
+    $('#lqz_refresh').click(() => {
+      document.location.reload();
+    });
+
   });
 
-  $('.record').click(() => {
-    window.location = "v_taskinfo.html";
-  });
+
+$(document).ajaxError(() => {
+  $('#status').html('Error: unknown ajaxError!');
 });
