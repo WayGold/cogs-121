99379b03d7531d4acfa2655f76d03593d8b72410
zsy <zsy@zsydeMacBook-Pro.local>
2019-05-26 01:20:26 -0700

    add check login in login.js and add drop down select form in v_task.html

--- static_files/js/login.js
+++ static_files/js/login.js
@@ -1,3 +1,56 @@
+function isPasswd(s) {
+  var patrn = /^(\w){6,20}$/;
+  if (!patrn.exec(s)) return false
+  return true
+}
+
+function isTrueName(s) {
+  var patrn = /^[a-zA-Z]{1,30}$/;
+  if (!patrn.exec(s))
+  return false
+  return true
+}
+
+
+function isMobil(s) {
+  var patrn = /^[+]{0,1}(\d){1,3}[ ]?([-]?((\d)|[ ]){1,12})+$/;
+  if (!patrn.exec(s)) return false
+  return true
+}
+function isPostalCode(s)
+{
+var patrn=/^[a-zA-Z0-9 ]{3,12}$/;
+if (!patrn.exec(s)) return false
+return true
+}
+
+function check_login() {
+
+  if (($('#zw_create_account_username').val()).length < 6) {
+    alert("username must be between 6 and 20 characters long");
+    return false;
+  }
+
+  if (!isPasswd($('#zw_create_account_pwd').val())) {
+    alert("password must be between 6 and 20 characters long");
+    return false
+  }
+
+  if (!isMobil($('#zw_create_account_phone').val())) {
+    alert("invaild phone number");
+    return false
+  }
+  if (!isTrueName($('#zw_create_account_name').val())) {
+    alert("invaild name");
+    return false
+  }
+
+  if (  !isPostalCode($('#zw_create_account_loc').val())) {
+    alert("invaild zipcode");
+    return false
+  }
+}
+
 $(document).ready(() => {
 
   $('#zw_login_btn').click(() => {
@@ -11,30 +64,29 @@ $(document).ready(() => {
       // all URLs are relative to http://localhost:3000/
       url: requestURL,
       type: 'GET',
-      dataType : 'json',
+      dataType: 'json',
       success: (data) => {
         console.log('You received some data!', data);
-        if(data){
-          if(!Object.keys(data).length){
+        if (data) {
+          if (!Object.keys(data).length) {
             console.log("User not found!");
             $('#zw_login_status').html('User not found!');
-          }
-          else{
+          } else {
             // Check for pwd correctness
             console.log("Evaluating pwd: " + pwd + " with " + data.password);
-            if(data.password == pwd){
-              console.log(username + 'login successfully!' );
+            if (data.password == pwd) {
+              console.log(username + 'login successfully!');
               // Set global user to login user
               current_user = username;
               localStorage.setItem("user", data.username);
               // Check user role
-              if(data.role == 1) // volunteer
+              if (data.role == 1) // volunteer
                 window.location = "/html/volunteer/v_task.html";
               else // requester
                 window.location = "/html/requester/r_record.html";
             }
             // Pwd not match
-            else{
+            else {
               $('#zw_login_status').html('Wrong password!');
             }
           }
@@ -45,33 +97,61 @@ $(document).ready(() => {
 
   $('#zw_create_account_btn').click(() => {
     console.log("create clicked!");
-
-    if(confirm("Please confirm your action")){
-      $.ajax({
-        // all URLs are relative to http://localhost:3000/
-        url: '../../create_user',
-        type: 'POST', // <-- this is POST, not GET
-        data: {
-          username: $('#zw_create_account_username').val(),
-          name: $('#zw_create_account_name').val(),
-          password: $('#zw_create_account_pwd').val(),
-          phone: $('#zw_create_account_phone').val(),
-          location: $('#zw_create_account_loc').val(),
-          role: $('#zw_create_account_role').val(),
-        },
-        success: (data) => {
-          console.log(data);
-          if(data == "Success") {
-            window.location = "../html/login.html";
+    if (check_login()) {
+      if (confirm("Please confirm your action")) {
+        $.ajax({
+          // all URLs are relative to http://localhost:3000/
+          url: '../../create_user',
+          type: 'POST', // <-- this is POST, not GET
+          data: {
+            username: $('#zw_create_account_username').val(),
+            name: $('#zw_create_account_name').val(),
+            password: $('#zw_create_account_pwd').val(),
+            phone: $('#zw_create_account_phone').val(),
+            location: $('#zw_create_account_loc').val(),
+            role: $('#zw_create_account_role').val(),
+          },
+          success: (data) => {
+            console.log(data);
+            if (data == "Success") {
+              window.location = "../html/login.html";
+            } else {
+              console.log("{message: 'error in app.post(/create_user), try another username'}");
+              $('#zw_create_status').html('Fail to create, try another username!');
+            }
           }
-
-          else {
-            console.log("{message: 'error in app.post(/create_user), try another username'}");
-            $('#zw_create_status').html('Fail to create, try another username!');
-          }
-        }
-      });
+        });
+      }
+    } else {
+      $('#zw_create_status').html('Fail to create');
     }
+
+    // if(confirm("Please confirm your action")){
+    //   $.ajax({
+    //     // all URLs are relative to http://localhost:3000/
+    //     url: '../../create_user',
+    //     type: 'POST', // <-- this is POST, not GET
+    //     data: {
+    //       username: $('#zw_create_account_username').val(),
+    //       name: $('#zw_create_account_name').val(),
+    //       password: $('#zw_create_account_pwd').val(),
+    //       phone: $('#zw_create_account_phone').val(),
+    //       location: $('#zw_create_account_loc').val(),
+    //       role: $('#zw_create_account_role').val(),
+    //     },
+    //     success: (data) => {
+    //       console.log(data);
+    //       if(data == "Success") {
+    //         window.location = "../html/login.html";
+    //       }
+    //
+    //       else {
+    //         console.log("{message: 'error in app.post(/create_user), try another username'}");
+    //         $('#zw_create_status').html('Fail to create, try another username!');
+    //       }
+    //     }
+    //   });
+    // }
 
   })
 })
